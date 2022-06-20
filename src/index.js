a84911d84c16bbc9d350b2d3ecfde9528c9c723b
hmacqueen19 <104796751+hmacqueen19@users.noreply.github.com>
2022-06-20 10:53:53 -0400

    Added appropriate javascript and added link to Git Hub repository

--- src/index.js
+++ src/index.js
@@ -1,101 +1,99 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor"
-      crossorigin="anonymous"
-    />
-    <link href="src/styles.css" rel="stylesheet" type="text/css"/>
-    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-  </head>
-    <title>Weather App</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
-   
-</head>
-  <body>
-    <div class="container">
-        <h1>Weather App Name</h1>
-        <form id="search-form">
-            <input id="location-bar" class="location-bar" type="text" placeholder="Enter your location">
-            <input id="search-button" class="search-button" type="submit" value="Search">
-            <input id="submit-button" class="current-location" type="submit" value="Current Location">
-        </form>
-        <div class="row">
-            <div class="col-6">
-                <div class="card current-weather">
-                    <div class="card-body">
-                        <h2 id="current-location" class="card-title"></h5>
-                        <h5><strong id="temperature-current"></strong>
-                            <a id="celsius" href="#">°C</a> |
-                            <a id="fahrenheit" href="#"> °F </a>
-                        </h3>
-                        <p id="current-date"></p>
-                        <p class="card-text" id="current-weather"></p>
-                    </div>
-                </div>
-            </div>
-        <div class="card daily-weather">    
-            <div class="row justify-content-centert">
-                <div class="col-md-2">
-                    <div class="card daily-forecast">
-                        <div class="card-body">
-                            <h5 class="card-title">Forecast Day 1</h5>
-                            <p class="card-text">Forecast will appear here</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="card daily-forecast">
-                        <div class="card-body">
-                            <h5 class="card-title">Forecast Day 2</h5>
-                            <p class="card-text">Forecast will appear here</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="card daily-forecast">
-                        <div class="card-body">
-                            <h5 class="card-title">Forecast Day 3</h5>
-                            <p class="card-text">Forecast will appear here</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="card daily-forecast">
-                        <div class="card-body">
-                            <h5 class="card-title">Forecast Day 4</h5>
-                            <p class="card-text">Forecast will appear here</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="card daily-forecast">
-                        <div class="card-body">
-                            <h5 class="card-title">Forecast Day 5</h5>
-                            <p class="card-text">Forecast will appear here</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>    
-            <div class="col-12">
-                <div class="card blanket-information" style="width: 100%">
-                    <div class="card-body">
-                        <h5 class="card-title">Yarn Color and Range</h5>
-                        <p class="card-text">Data will appear here</p>
-                    </div>
-                </div>
-            </div> 
-        </div>
-       <a href="https://github.com/hmacqueen19/my-weather-application">Open-source code by Helen Macqueen</a>
-       <script src="src/index.js"></script>  
-  </body>
-</html>
+function getTodaysFormattedDate() {
+  /**
+   * returns today's date and time in a specific format
+   */
+  let dayWords = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let today = new Date();
+  let currentDay = dayWords[today.getDay()];
+  let currentHour = today.getHours();
+  let currentMinute = today.getMinutes();
+
+  return `${currentDay} ${currentHour}:${currentMinute}`;
+}
+function displayCityAndTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureCurrent = document.querySelector("#temperature-current");
+  temperatureCurrent.innerHTML = temperature;
+  // Set the targets innerHTML
+  // let searchInput = document.querySelector("#location-bar");
+  let cityName = document.querySelector("#current-location");
+  cityName.innerHTML = response.data.name;
+}
+function getTemperature(cityName) {
+  let apiKey = "6235b58538eef339ac44773d06f22df9";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
+
+  axios
+    .get(apiUrl)
+    .then(displayCityAndTemperature)
+    .catch((error) => {
+      if (error.response.status === 404) {
+        alert("Please enter a valid city name.");
+      }
+    });
+}
+
+function searchLocation(position) {
+  let apiKey = "6235b58538eef339ac44773d06f22df9";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayCityAndTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+let currentLocationButton = document.querySelector("#submit-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+//=====================================
+
+// get the HTML reference
+let updatedDate = document.querySelector("#current-date");
+
+// set the innerHTML from step 1 with function I made
+updatedDate.innerHTML = getTodaysFormattedDate();
+
+function search(event) {
+  event.preventDefault();
+  // Get the search input from input element.
+  let searchInput = document.querySelector("#location-bar");
+
+  if (searchInput.value.trim().length === 0) {
+    alert("Please enter a location");
+    return;
+  }
+  getTemperature(searchInput.value);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+// /**
+//  * Change temperature to celsius
+//  */
+// function showCelsius(event) {
+//   event.preventDefault();
+//   let celsius = document.querySelector("#temperature-current");
+//   celsius.innerHTML = "23";
+// }
+// let celsiusTemperature = document.querySelector("#celsius");
+// celsiusTemperature.addEventListener("click", showCelsius);
+// /**
+//  * Change temperature to Fahrenheit
+//  */
+// function showFahrenheit(event) {
+//   event.preventDefault();
+//   let fahrenheit = document.querySelector("#temperature-current");
+//   fahrenheit.innerHTML = "81";
+// }
+// let fahrenheitTemperature = document.querySelector("#fahrenheit");
+// fahrenheitTemperature.addEventListener("click", showFahrenheit);
